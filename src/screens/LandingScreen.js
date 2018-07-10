bd7052e691fcdbe556f2b46b9b8fcc9917471042
Arnaud Ayrem <arnaud.ayrem@gmail.com>
2018-07-10 10:30:18 +0200

    Implemented token expiration checking

--- src/screens/LandingScreen.js
+++ src/screens/LandingScreen.js
@@ -15,10 +15,24 @@ class LandingScreen extends React.Component {
         return JSON.parse(window.atob(token.split('.')[1].replace('-', '+').replace('_', '/')));
     };
 
+    isTokenValid = (token) => {
+        const claims = this.decodeToken(token);
+        const expDate = new Date(parseInt(claims.exp) * 1000);
+        return expDate > new Date();
+    };
 
     componentDidMount(){
         if(this.props.token === null ){
             this.props.navigation.navigate('Login');
+        }else{
+            //Check token expired
+            const isValid = this.isTokenValid(this.props.token);
+            //if token
+            if(isValid){
+                this.props.navigation.navigate('Main');
+            }else{
+                this.props.navigation.navigate('Login');
+            }
         }
     }
 
@@ -31,11 +45,16 @@ class LandingScreen extends React.Component {
 
             const claims = this.decodeToken(nextProps.token);
             console.log(claims);
+            const exp = new Date(claims.exp);
+            console.log(exp);
             //Check token expired
+            const isValid = this.isTokenValid(this.props.token);
             //if token
-            this.props.navigation.navigate('Main');
-            //else
-            //this.props.navigation.navigate('Login');
+            if(isValid){
+                this.props.navigation.navigate('Main');
+            }else{
+                this.props.navigation.navigate('Login');
+            }
         }else{
             console.log('la');
 
@@ -48,6 +67,7 @@ class LandingScreen extends React.Component {
     }
 
     render() {
+        console.log(this.props);
         return <Spinner color="blue"/>
     }
 }

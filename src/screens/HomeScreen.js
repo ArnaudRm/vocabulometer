df205094a539045c2c0f52dad94c58bb534be9b0
Timothée Binet <timothee.binet@etu-u.bordeaux.fr>
2018-07-11 16:29:50 +0200

    Home page finished

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,12 +11,12 @@ import {
     Col,
     Row,
     Icon,
+    Card,
+    CardItem,
+    Body,
 } from 'native-base';
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-    },
     title: {
         paddingVertical: 24,
     },
@@ -28,7 +28,34 @@ const styles = StyleSheet.create({
         marginBottom: 16,
     },
     btn: {
-        marginTop: 8,
+        width: '100%',
+        justifyContent: 'flex-start'
+    },
+    btnRevise: {
+        backgroundColor: '#FFD43A'
+    },
+    btnEasy: {
+        backgroundColor: '#20B449'
+    },
+    btnHard: {
+        backgroundColor: '#FF473A',
+
+    },
+    btnProfile: {
+        backgroundColor: '#6FB98F',
+    },
+    labelGray: {
+        color: '#979797',
+        fontSize: 10
+    },
+    paddingButton: {
+        paddingHorizontal: 20,
+        paddingVertical: 0,
+        marginVertical: 0,
+        maxHeight: 60,
+    },
+    txtStat: {
+        fontSize: 12,
     }
 });
 
@@ -38,63 +65,95 @@ export default class HomeScreen extends React.Component {
         return (
             <Container>
                 <Content padder contentContainerStyle={styles.container}>
-                    <Grid style={{minHeight: 256}}>
-                        <Row size={30}>
-                            <H1>Hello!</H1>
+                {/* PROFILE */}
+                  <Card paddingBottom={20}>
+                    <CardItem>
+                    <Grid style={{minHeight: 60}}>
+                        <Row size={10}>
+                          <Text style={styles.labelGray}>PROFILE</Text>
                         </Row>
-                        <Row size={25}>
-                            <H3>Bienvenue sur Vocabulometer</H3>
+                        <Row size={20} paddingBottom={10} paddingTop={10}>
+                            <H1>See your statistics !</H1>
                         </Row>
-                        <Row size={45}>
-                            <Text>
-                                Vocabulometer a pour objectif d'aider à l'apprentissage de l'anglais en offrant une
-                                plateforme permettant d'enrichir son vocabulaire.
-                            </Text>
+                    </Grid>
+                    </CardItem>
+                    <CardItem>
+
+                    <Grid style={{minHeight: 80}}>
+                      <Row size={80}>
+                        <Col size={40} >
+                          <Button
+                              style={[styles.btn,styles.btnProfile]}
+                          >
+                              <Text>My profile</Text>
+                          </Button>
+                        </Col>
+                        <Col size={60} paddingLeft={20}>
+                            <Text style={styles.txtStat}>Score:  <Text style={{fontWeight: 'bold'}}>10</Text> points</Text>
+                            <Text style={styles.txtStat}>Words read: <Text style={{fontWeight: 'bold'}}>542</Text></Text>
+                            <Text style={styles.txtStat}>New words encountered: <Text style={{fontWeight: 'bold'}}>15</Text></Text>
+                            <Text style={styles.txtStat}>New recently read words: <Text style={{fontWeight: 'bold'}}>152</Text></Text>
+                        </Col>
+                      </Row>
+                    </Grid>
+
+                    </CardItem>
+                </Card>
+
+                {/* RECOMMENDATIONS */}
+                <Card paddingBottom={20}>
+                <CardItem>
+                    <Grid style={{minHeight: 60}}>
+                        <Row size={10}>
+                          <Text style={styles.labelGray}>RECOMMANDATIONS</Text>
+                        </Row>
+                        <Row size={20} paddingBottom={10} paddingTop={10}>
+                            <H1>Train your english skills with vocabulometer !</H1>
                         </Row>
                     </Grid>
 
+                    </CardItem>
+                    <CardItem style={styles.paddingButton}>
+                      <Button
+                          full
+                          style={[styles.btn,styles.btnRevise]}
+                          onPress={() => this.props.navigation.navigate('EasyTexts')}
+                      >
+                          <Icon
+                              type="FontAwesome"
+                              name='thermometer-empty'
+                          />
+                          <Text>Revise</Text>
+                      </Button>
+                    </CardItem>
+                    <CardItem style={styles.paddingButton}>
                     <Button
                         full
-                        iconLeft
-                        success
-                        rounded
-                        style={styles.btn}
-                        onPress={() => this.props.navigation.navigate('EasyTexts')}
-                    >
-                        <Icon
-                            type="FontAwesome"
-                            name='thermometer-empty'
-                        />
-                        <Text>Textes faciles</Text>
-                    </Button>
-                    <Button
-                        full
-                        iconLeft
-                        primary
-                        rounded
-                        style={styles.btn}
+                        style={[styles.btn,styles.btnEasy]}
                         onPress={() => this.props.navigation.navigate('RecommendedTexts')}
                     >
                         <Icon
                             type="FontAwesome"
                             name='thermometer-half'
                         />
-                        <Text>Textes recommandés</Text>
+                        <Text>Easy</Text>
                     </Button>
+                    </CardItem>
+                    <CardItem style={styles.paddingButton}>
                     <Button
                         full
-                        iconLeft
-                        danger
-                        rounded
-                        style={styles.btn}
+                        style={[styles.btn,styles.btnHard]}
                         onPress={() => this.props.navigation.navigate('HardTexts')}
                     >
                         <Icon
                             type="FontAwesome"
                             name='thermometer-full'
+                            padding={50}
                         />
-                        <Text>Hardcore</Text>
+                        <Text>Hard</Text>
                     </Button>
+                    </CardItem>
+                    </Card>
                 </Content>
             </Container>
         );
